47066829d617aae0a4337e20273b2efb671903a5
chash-code <panchalchash@gmail.com>
2026-01-13 23:58:30 +0530

    index.js

---
+++ question-2/index.js
@@ -0,0 +1,9 @@
+// Import the checkPrime function from math.js
+import { checkPrime } from './math.js';
+
+// Test the function with different values
+console.log(checkPrime(7));    // true - 7 is prime
+console.log(checkPrime(10));   // false - 10 is not prime
+console.log(checkPrime(13));   // true - 13 is prime
+console.log(checkPrime(1));    // false - 1 is not prime
+console.log(checkPrime(29));   // true - 29 is prime
